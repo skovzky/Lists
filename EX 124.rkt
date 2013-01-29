aae97c96b7c228bac6943734504aa38dc1adf6e7
Robert <administrator@Roberts-MacBook.local>
2013-01-28 22:01:46 -0600

    EX 124
    Creation and Minimal Start

---
+++ EX 124.rkt
@@ -0,0 +1,7 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |EX 124|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;EX 124
+;robert heemann (THE GREAT VLADIMIR SKOVZKY)
+
+
